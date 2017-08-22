b7a5d668d889bef79161dcedf976a19fb4adf586
Egraz Marlene <marlene.egraz@gmail.com>
2017-08-22 11:51:09 +0200

    add test and correct

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
- var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
- function fetch(url) {
-     return new Promise(function(ok, ko) {
-         let xhr = new XMLHttpRequest();
-         xhr.open("GET", url);
-         xhr.onload = function() {
-             if (xhr.status == 200) {
-                 ok(xhr.response);
-             } else {
-                 ko(xhr.status)
-             }
-         };
-         xhr.onerror = function(error) {
-             ko(error);
-         }
-         xhr.send();
-     });
- };
- module.exports = fetch;
+function fetch(url) {
+    return new Promise(function(resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.onload = function() {
+            if (xhr.status == 200) {
+                resolve(xhr.responseText);
+            } else {
+                reject(xhr.status)
+            }
+        };
+        xhr.onerror = function(error) {
+            reject(error);
+        }
+        xhr.send();
+    });
+};
+module.exports = fetch;
